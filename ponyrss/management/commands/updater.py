c0c7e835203e0a191d26ec4245c901bfd2f56e6d
palkeo <contact@palkeo.com>
2014-08-02 12:59:13 -0300

    Entry deletion (commented out for now)

--- ponyrss/management/commands/updater.py
+++ ponyrss/management/commands/updater.py
@@ -2,6 +2,11 @@ from django.core.management.base import BaseCommand
 from ponyrss import models
 from ponyrss import spouts
 from multiprocessing import Pool
+from django.db.models import Q
+import datetime
+
+KEEP_READ_ARTICLE_FOR = datetime.timedelta(days=15)
+KEEP_UNREAD_ARTICLE_FOR = datetime.timedelta(days=30)
 
 def update_feed(feed):
     print("Updating %s" % feed)
@@ -12,6 +17,15 @@ class Command(BaseCommand):
     help = "Update everything"
 
     def handle(self, feed_id=None, **kwargs):
+        # Delete old entries
+
+        #Qread = Q(read=True, date__lt=(datetime.datetime.now() - KEEP_READ_ARTICLE_FOR))
+        #Qunread = Q(read=False, date__lt=(datetime.datetime.now() - KEEP_UNREAD_ARTICLE_FOR))
+
+        #models.Entry.objects.filter(Qread | Qunread).delete()
+
+        # Add new entries
+
         if feed_id is None:
             queryset = models.Feed.objects.all()
         else:
